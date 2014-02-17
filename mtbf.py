6029a6f576a9ad700f675df1d1d5b1c8dc9bba3d
pyang <pyang@Paul-Desktop.(none)>
2014-02-17 11:10:04 +0800

    add time input

--- mtbf.py
+++ mtbf.py
@@ -3,9 +3,26 @@ import os
 import sys
 import signal
 import time
+import re
 
 from gaiatest.runtests import GaiaTestRunner, GaiaTestOptions
-from marionette.runtests import startTestRunner
+
+
+def time2sec(input_str):
+    day = check_none_group(re.search('[^0-9]*([0-9]+)[Dd]', input_str))
+    hour = check_none_group(re.search('[^0-9]*([0-9]+)[Hh]', input_str))
+    minute = check_none_group(re.search('[^0-9]*([0-9]+)[Mm]', input_str))
+    second = check_none_group(re.search('[^0-9]*([0-9]+)[Ss]', input_str))
+    ret = ((day * 24 + hour) * 60 + minute) * 60 + second
+    if(ret > 0):
+        return ret
+    raise ValueError
+
+
+def check_none_group(m):
+    if m is None:
+        return 0
+    return int(m.group(1))
 
 
 class MTBF_Driver:
@@ -27,14 +44,17 @@ class MTBF_Driver:
         ## Infinite run before time expired
         runner_class = GaiaTestRunner
         parser_class = GaiaTestOptions
-        parser = parser_class(usage='%prog [options] test_file_or_dir <test_file_or_dir> ...')
+        parser = parser_class(
+            usage='%prog [options] test_file_or_dir <test_file_or_dir> ...'
+        )
         options, tests = parser.parse_args()
         parser.verify_usage(options, tests)
         self.start_time = time.time()
 
         while(True):
             ## Run test
-            ## workaround: kill the runner and create another one each round, should be fixed
+            ## workaround: kill the runner and create another
+            ## one each round, should be fixed
             self.runner = runner_class(**vars(options))
             self.runner.run_tests(tests)
             self.passed = self.runner.passed + self.passed
@@ -44,9 +64,10 @@ class MTBF_Driver:
             self.logger = logging.getLogger('Marionette')
             self.logger.handlers = []
 
-            # This is a temporary solution for stop the tests
-            # If there should be any interface there for us to detect continuous failure
-            # We can then remove this
+            ## This is a temporary solution for stop the tests
+            ## If there should be any interface there for us
+            ## to detect continuous failure We can then
+            ## remove this
             if self.runner.passed == 0:
                 break
 
@@ -59,14 +80,25 @@ class MTBF_Driver:
         self.runner.logger.info('todo:   %d' % self.todo)
 
     def time_up(self, signum, frame):
-        print ("Signal handler called with signal", signum)
+        print(
+            "Signal handler called with signal",
+            signum
+        )
         self.get_report()
-        os._exit(0)
+        #os._exit(0)
+        sys.exit(0)
 
 
 def main():
     ## set default as 2 mins
-    time = int(os.getenv('MTBF_TIME', 2)) * 60
+    try:
+        time = int(time2sec(os.getenv('MTBF_TIME', '2m')))
+    except ValueError:
+        print(
+            "input value parse error: ",
+            os.getenv('MTBF_TIME'),
+            ", format should be '1d', '10h', '10m50s'"
+        )
     mtbf = MTBF_Driver(time)
 
     signal.signal(signal.SIGALRM, mtbf.time_up)
