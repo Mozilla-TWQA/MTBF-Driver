e6eefdc162f6290647056172d4d86e278ebb0c96
Mozilla-TPE <mozilla-tpe@Mozilla-TPE-pyang-MacBook-Air.local>
2014-04-16 16:07:15 +0800

    pep8 fix; add item in gitignore

---
+++ utils/step_recorder.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+class StepRecorder(object):
+    def __init__(self):
+        pass
+
+    def replay(self):
+        pass
+    
+    def set_output(self, fd):
+        pass
+
+    def save(self):
+        pass
+
+    def rotate(self):
+        pass
