1371f645174c5e7d30a6f1c84048b3a48c622652
Paul Zapion Yang <zapionator@gmail.com>
2014-05-08 19:39:40 +0800

    fix pep8 warning

--- mtbf-driver/mtbf.py
+++ mtbf-driver/mtbf.py
@@ -142,19 +142,19 @@ class MTBF_Driver:
                 tools.get_about_memory.get_and_show_info(memory_report_args())
 
             ## get logcat and dmesg
-            if self.conf.has_key('logcat') and self.conf['logcat']:
+            if 'logcat' not in self.conf and self.conf['logcat']:
                 logcat_cmd = "adb logcat -v threadtime -d > logcat" + str(current_round)
                 dmesg_cmd = "adb shell dmesg > dmesg" + str(current_round)
                 os.system(logcat_cmd)
                 os.system(dmesg_cmd)
 
             ## show us the overall status of the phone
-            if self.conf.has_key('overall_status') and self.conf['overall_status']:
+            if 'overall_status' not in self.conf and self.conf['overall_status']:
                 bugreport_cmd = "adb shell dumpstate > bugreport" + str(current_round)
                 os.system(bugreport_cmd)
 
             ## show us b2g status of the phone
-            if self.conf.has_key('b2g_status') and self.conf['b2g_status']:
+            if 'b2g_status' not in self.conf and self.conf['b2g_status']:
                 b2ginfo_cmd = "adb shell b2g-info -t > b2ginfo" + str(current_round)
                 b2gps_cmd = "adb shell b2g-ps -t -p -P --oom > b2gps" + str(current_round)
                 b2gprocrank_cmd = "adb shell b2g-procrank --oom > b2gprocrank" + str(current_round)
@@ -163,7 +163,7 @@ class MTBF_Driver:
                 os.system(b2gprocrank_cmd)
 
             ## show us events
-            if self.conf.has_key('get_event') and self.conf['get_event']:
+            if 'get_event' not in self.conf and self.conf['get_event']:
                 bugreport_cmd = "adb shell getevent -S > getevent" + str(current_round)
                 os.system(bugreport_cmd)
 
