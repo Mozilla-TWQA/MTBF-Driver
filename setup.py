a441822ea3813eea337a10e6ea12b84a8e863b72
Walter C <wachen@mozilla.com>
2014-05-12 11:28:29 +0800

    Adding setup script

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mtbf-driver",
+    version="0.1.0",
+    packages = find_packages(),
+    
+    description="mtbf package",
+    author="Mozilla Taiwan",
+    author_email="tw-qa@mozilla.com",
+)
+
