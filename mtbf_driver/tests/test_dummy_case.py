9eddfac25145c4141a0410474df5beb3dc70930d
Walter C <wachen@mozilla.com>
2014-09-24 15:09:12 +0800

    added 15 seconds for waiting

--- mtbf_driver/tests/test_dummy_case.py
+++ mtbf_driver/tests/test_dummy_case.py
@@ -17,6 +17,7 @@ class DummyTestCase(GaiaMtbfTestCase):
 
     def test_status_check(self):
         self._check_page_source()
+        time.sleep(15)
         self.assertEqual(1,1)
         self._check_cpu_load()
 
