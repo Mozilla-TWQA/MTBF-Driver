a26fdd85d0be6bb05ff8af64ba667052c0fc4084
shako <sho@mozilla.com>
2015-03-03 14:58:00 +0800

    fix contact edit dialog opened before launch contacts
    Fix contact finding without render first..
    
    fix the cancel search button can't find before touch the search input field
    
    modify the locator more readable and change the checking mechanism for existing edit form

---
+++ mtbf_driver/mtbf_apps/contacts/app.py
@@ -0,0 +1,57 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from marionette.by import By
+from marionette import expected
+from marionette.errors import JavascriptException
+from gaiatest.apps.contacts.app import Contacts
+from marionette import Wait
+from gaiatest.apps.base import Base
+
+
+class MTBF_Contacts(Contacts):
+
+    _edit_form_locator = (By.ID, 'view-contact-form')
+    _edit_form_close_locator = (By.ID, 'contact-form-header')
+    _cancel_search_button_locator = (By.ID, 'cancel-search')
+    _search_field_area_locator = (By.CSS_SELECTOR, 'input[data-l10n-id="search-contact"]')
+    _search_field_content_locator = (By.ID, 'search-contact')
+    _search_result_contact_locator = (By.CSS_SELECTOR, '#search-list > li.contact-item > p.contact-text ')
+
+    def launch(self):
+        Base.launch(self)
+
+        Wait(self.marionette, ignored_exceptions=JavascriptException).until(
+            lambda m: m.execute_script('return window.wrappedJSObject.Contacts.asyncScriptsLoaded;') is True)
+
+        self.close_existing_edit_form()
+
+        self.clear_search_view()
+
+        Wait(self.marionette).until(expected.element_displayed(
+            Wait(self.marionette).until(expected.element_present(
+                *self._settings_button_locator))))
+
+    def clear_search_view(self):
+        if len(self.marionette.find_elements(*self._cancel_search_button_locator)) != 0:
+            cancel_search_btn = self.marionette.find_element(*self._cancel_search_button_locator)
+            if cancel_search_btn.is_displayed():
+                cancel_search_btn.tap()
+
+    def close_existing_edit_form(self):
+        if len(self.marionette.find_elements(*self._edit_form_locator)) != 0:
+            edit_form = self.marionette.find_element(*self._edit_form_locator)
+            if "current" in edit_form.get_attribute('class'):
+                self.apps.switch_to_displayed_app()
+                self.marionette.execute_async_script("Accessibility.click(arguments[0].shadowRoot.querySelector('button.action-button'));",
+                                                     [self.marionette.find_element(*self._edit_form_close_locator)],
+                                                     special_powers=True)
+
+    def search_contact(self, search_no):
+        search_field_area = self.marionette.find_element(*self._search_field_area_locator)
+        search_field_area.tap()
+        search_field_content = self.marionette.find_element(*self._search_field_content_locator)
+        search_field_content.send_keys(search_no)
+        find_contact = self.marionette.find_element(*self._search_result_contact_locator)
+        return self.Contact(self.marionette, find_contact)
